da51ea91ec8d237db6a68893aa1602c50dcc0841
yaonimblaboratory <yao.nimb.laboratory@gmail.com>
2025-12-23 19:05:56 +0800

    Set base path to './' in Vite config
    Set base path for relative builds to avoid URL issues.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,5 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Using './' makes the build relative, so it works regardless of the repo name
+  // and avoids issues with the "Sync" tool trying to guess your URL.
   base: './', 
 });
